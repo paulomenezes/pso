e7f9eda9b75c60990e827236a980aecfa1ac2697
Paulo Menezes <phgm@cesar.org.br>
2017-08-23 07:50:31 -0300

    PSO

--- index.js
+++ index.js
@@ -3,6 +3,20 @@ exports.__esModule = true;
 var particle_1 = require("./particle");
 var position_1 = require("./position");
 var velocity_1 = require("./velocity");
+var SWARM_SIZE = 100;
+var MAX_ITERATION = 10000;
+var PROBLEM_DIMENSION = 2;
+var C1 = 2.0;
+var C2 = 2.0;
+var W_UPPERBOUND = 1.0;
+var W_LOWERBOUND = 0.0;
+var LOC_X_LOW = 1;
+var LOC_X_HIGH = 4;
+var LOC_Y_LOW = -1;
+var LOC_Y_HIGH = 1;
+var VEL_LOW = -1;
+var VEL_HIGH = 1;
+var ERR_TOLERANCE = 0.001;
 var PSO = (function () {
     function PSO() {
         this.swarm = [];
@@ -13,37 +27,104 @@ var PSO = (function () {
     }
     PSO.prototype.initializeSwam = function () {
         var particle;
-        for (var i = 0; i < 30; i++) {
-            var posX = Math.random() * 3.0 + 1.0;
-            var posY = Math.random() * 2.0 - 1.0;
-            var velX = Math.random() * 2.0 - 1.0;
-            var velY = Math.random() * 2.0 - 1.0;
+        for (var i = 0; i < SWARM_SIZE; i++) {
+            var posX = LOC_X_LOW + Math.random() * LOC_X_HIGH - LOC_X_LOW;
+            var posY = LOC_Y_LOW + Math.random() * LOC_Y_HIGH - LOC_Y_LOW;
+            var velX = VEL_LOW + Math.random() * VEL_HIGH - VEL_LOW;
+            var velY = VEL_LOW + Math.random() * VEL_HIGH - VEL_LOW;
             particle = new particle_1.Particle();
             particle.position = new position_1.Position(posX, posY);
             particle.velocity = new velocity_1.Velocity(velX, velY);
             this.swarm.push(particle);
         }
     };
-    PSO.prototype.execute = function () {
+    PSO.prototype.updateFitness = function () {
         var _this = this;
-        this.initializeSwam();
-        this.swarm.forEach(function (particle) {
-            _this.fitnessValueList.push(particle.getFitness());
+        this.swarm.forEach(function (particle, index) {
+            _this.fitnessValueList[index] = particle.getFitness();
         });
-        for (var i = 0; i < this.swarm.length; i++) {
+    };
+    PSO.prototype.getMinPos = function (list) {
+        var pos = 0;
+        var minValue = list[0];
+        for (var i = 0; i < list.length; i++) {
+            if (list[i] < minValue) {
+                pos = i;
+                minValue = list[i];
+            }
+        }
+        return pos;
+    };
+    PSO.prototype.evaluate = function (position) {
+        var result = 0;
+        var x = position.x; // the "x" part of the location
+        var y = position.y; // the "y" part of the location
+        result =
+            Math.pow(2.8125 - x + x * Math.pow(y, 4), 2) +
+                Math.pow(2.25 - x + x * Math.pow(y, 2), 2) +
+                Math.pow(1.5 - x + x * y, 2);
+        return result;
+    };
+    PSO.prototype.execute = function () {
+        this.initializeSwam();
+        this.updateFitness();
+        for (var i = 0; i < SWARM_SIZE; i++) {
             this.pBest[i] = this.fitnessValueList[i];
             this.pBestLocation.push(this.swarm[i].position);
         }
         var t = 0;
         var w = 0;
         var err = 9999;
-        while (t < 100) {
-            for (var i = 0; i < this.swarm.length; i++) {
-                this.pBest[i] = this.fitnessValueList[i];
-                this.pBestLocation[i] = this.swarm[i].position;
+        while (t < MAX_ITERATION && err > ERR_TOLERANCE) {
+            // Update pBest
+            for (var i = 0; i < SWARM_SIZE; i++) {
+                if (this.fitnessValueList[i] < this.pBest[i]) {
+                    this.pBest[i] = this.fitnessValueList[i];
+                    this.pBestLocation[i] = this.swarm[i].position;
+                }
             }
+            // Update gBest
+            var bestParticleIndex = this.getMinPos(this.fitnessValueList);
+            if (t === 0 || this.fitnessValueList[bestParticleIndex] < this.gBest) {
+                this.gBest = this.fitnessValueList[bestParticleIndex];
+                this.gBestLocation = this.swarm[bestParticleIndex].position;
+            }
+            w = W_UPPERBOUND - t / MAX_ITERATION * (W_UPPERBOUND - W_LOWERBOUND);
+            for (var i_1 = 0; i_1 < SWARM_SIZE; i_1++) {
+                var r1 = Math.random();
+                var r2 = Math.random();
+                var p = this.swarm[i_1];
+                // Update velocity
+                var newVel = [];
+                newVel[0] =
+                    w * p.velocity.x +
+                        r1 * C1 * (this.pBestLocation[i_1].x - p.position.x) +
+                        r2 * C2 * (this.gBestLocation.x - p.position.x);
+                newVel[1] =
+                    w * p.velocity.y +
+                        r1 * C1 * (this.pBestLocation[i_1].y - p.position.y) +
+                        r2 * C2 * (this.gBestLocation.y - p.position.y);
+                var velocity = new velocity_1.Velocity(newVel[0], newVel[1]);
+                p.velocity = velocity;
+                // Update position
+                var newPos = [];
+                newPos[0] = p.position.x - newVel[0];
+                newPos[1] = p.position.y - newVel[1];
+                var position = new position_1.Position(newPos[0], newPos[1]);
+                p.position = position;
+            }
+            err = this.evaluate(this.gBestLocation) - 0;
+            console.log('Interation', t);
+            console.log('Best X', this.gBestLocation.x);
+            console.log('Best Y', this.gBestLocation.y);
+            console.log('Value', this.evaluate(this.gBestLocation));
             t++;
+            this.updateFitness();
         }
+        console.log('\nSolution found in interaction', t - 1);
+        console.log('Best X', this.gBestLocation.x);
+        console.log('Best Y', this.gBestLocation.y);
+        console.log('Error', err);
     };
     return PSO;
 }());
--- index.ts
+++ index.ts
@@ -2,13 +2,21 @@ import { Particle } from './particle';
 import { Position } from './position';
 import { Velocity } from './velocity';
 
-const SWARM_SIZE = 30;
-const MAX_ITERATION = 100;
-const PROBLEM_DIMENSION = 2;
+const SWARM_SIZE = 100;
+const MAX_ITERATION = 10000;
 const C1 = 2.0;
 const C2 = 2.0;
 const W_UPPERBOUND = 1.0;
 const W_LOWERBOUND = 0.0;
+
+const LOC_X_LOW = 1;
+const LOC_X_HIGH = 4;
+const LOC_Y_LOW = -1;
+const LOC_Y_HIGH = 1;
+const VEL_LOW = -1;
+const VEL_HIGH = 1;
+
+const ERR_TOLERANCE = 0.001;
 
 class PSO {
   swarm: Particle[] = [];
@@ -28,11 +36,11 @@ class PSO {
     let particle;
 
     for (var i = 0; i < SWARM_SIZE; i++) {
-      const posX = Math.random() * 3.0 + 1.0;
-      const posY = Math.random() * 2.0 - 1.0;
+      const posX = LOC_X_LOW + Math.random() * LOC_X_HIGH - LOC_X_LOW;
+      const posY = LOC_Y_LOW + Math.random() * LOC_Y_HIGH - LOC_Y_LOW;
 
-      const velX = Math.random() * 2.0 - 1.0;
-      const velY = Math.random() * 2.0 - 1.0;
+      const velX = VEL_LOW + Math.random() * VEL_HIGH - VEL_LOW;
+      const velY = VEL_LOW + Math.random() * VEL_HIGH - VEL_LOW;
 
       particle = new Particle();
       particle.position = new Position(posX, posY);
@@ -42,14 +50,44 @@ class PSO {
     }
   }
 
+  updateFitness() {
+    this.swarm.forEach((particle, index) => {
+      this.fitnessValueList[index] = particle.getFitness();
+    });
+  }
+
+  getMinPos(list: number[]) {
+    let pos = 0;
+    let minValue = list[0];
+
+    for (let i = 0; i < list.length; i++) {
+      if (list[i] < minValue) {
+        pos = i;
+        minValue = list[i];
+      }
+    }
+
+    return pos;
+  }
+
+  evaluate(position: Position) {
+    let result = 0;
+    let x = position.x; // the "x" part of the location
+    let y = position.y; // the "y" part of the location
+
+    result =
+      Math.pow(2.8125 - x + x * Math.pow(y, 4), 2) +
+      Math.pow(2.25 - x + x * Math.pow(y, 2), 2) +
+      Math.pow(1.5 - x + x * y, 2);
+
+    return result;
+  }
+
   execute() {
     this.initializeSwam();
+    this.updateFitness();
 
-    this.swarm.forEach(particle => {
-      this.fitnessValueList.push(particle.getFitness());
-    });
-
-    for (var i = 0; i < this.swarm.length; i++) {
+    for (var i = 0; i < SWARM_SIZE; i++) {
       this.pBest[i] = this.fitnessValueList[i];
       this.pBestLocation.push(this.swarm[i].position);
     }
@@ -58,20 +96,69 @@ class PSO {
     let w = 0;
     let err = 9999;
 
-    while (t < 100) {
-      for (var i = 0; i < this.swarm.length; i++) {
-        this.pBest[i] = this.fitnessValueList[i];
-        this.pBestLocation[i] = this.swarm[i].position;
+    while (t < MAX_ITERATION && err > ERR_TOLERANCE) {
+      // Update pBest
+      for (var i = 0; i < SWARM_SIZE; i++) {
+        if (this.fitnessValueList[i] < this.pBest[i]) {
+          this.pBest[i] = this.fitnessValueList[i];
+          this.pBestLocation[i] = this.swarm[i].position;
+        }
       }
 
-      let bestParticleIndex = 0;
+      // Update gBest
+      let bestParticleIndex = this.getMinPos(this.fitnessValueList);
       if (t === 0 || this.fitnessValueList[bestParticleIndex] < this.gBest) {
         this.gBest = this.fitnessValueList[bestParticleIndex];
         this.gBestLocation = this.swarm[bestParticleIndex].position;
       }
 
+      w = W_UPPERBOUND - t / MAX_ITERATION * (W_UPPERBOUND - W_LOWERBOUND);
+
+      for (let i = 0; i < SWARM_SIZE; i++) {
+        let r1 = Math.random();
+        let r2 = Math.random();
+
+        let p = this.swarm[i];
+
+        // Update velocity
+        let newVel = [];
+        newVel[0] =
+          w * p.velocity.x +
+          r1 * C1 * (this.pBestLocation[i].x - p.position.x) +
+          r2 * C2 * (this.gBestLocation.x - p.position.x);
+
+        newVel[1] =
+          w * p.velocity.y +
+          r1 * C1 * (this.pBestLocation[i].y - p.position.y) +
+          r2 * C2 * (this.gBestLocation.y - p.position.y);
+
+        let velocity = new Velocity(newVel[0], newVel[1]);
+        p.velocity = velocity;
+
+        // Update position
+        let newPos = [];
+        newPos[0] = p.position.x - newVel[0];
+        newPos[1] = p.position.y - newVel[1];
+
+        let position = new Position(newPos[0], newPos[1]);
+        p.position = position;
+      }
+
+      err = this.evaluate(this.gBestLocation) - 0;
+
+      console.log('Interation', t);
+      console.log('Best X', this.gBestLocation.x);
+      console.log('Best Y', this.gBestLocation.y);
+      console.log('Value', this.evaluate(this.gBestLocation));
+
       t++;
+      this.updateFitness();
     }
+
+    console.log('\nSolution found in interaction', t - 1);
+    console.log('Best X', this.gBestLocation.x);
+    console.log('Best Y', this.gBestLocation.y);
+    console.log('Error', err);
   }
 }
 
